6d46792b0bf3dda417f8afaeea43c5a6f691c170
Rizal amarulloh <rizalamarulloh2014@gmail.com>
2026-02-17 12:49:34 +0700

    feat: implement JWT utility and UserDetailsService

--- src/main/java/com/enigma/student_management/repository/EnrollmentRepository.java
+++ src/main/java/com/enigma/student_management/repository/EnrollmentRepository.java
@@ -1,4 +1,25 @@
 package com.enigma.student_management.repository;
 
-public interface EnrollmentRepository {
+import com.enigma.student_management.entities.Enrollment;
+import com.enigma.student_management.entities.EnrollmentStatus;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface EnrollmentRepository extends JpaRepository<Enrollment, UUID> {
+    // Check double enrollment
+    boolean existsByStudentIdAndCourseIdAndStatus(UUID studentId, UUID courseId, EnrollmentStatus status);
+
+    // Get student's enrollment
+    List<Enrollment> findByStudentIdOrderByCreatedAtDesc(UUID studentId);
+
+    // Get enrollment for grade assignment
+    Optional<Enrollment> findStudentIdAndCourseId(UUID studentId, UUID courseId);
+
+    // Get enrollments by course
+    List<Enrollment> findByCourseId(UUID courseId);
 }
